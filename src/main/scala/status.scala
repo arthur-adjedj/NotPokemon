45dc78e3ea142f0a453b2ecfa08909ced6d275d9
zBlxst <tomloulou@yahoo.fr>
2022-01-29 17:42:44 +0100

    imp status

--- src/main/scala/status.scala
+++ src/main/scala/status.scala
@@ -2,6 +2,9 @@ abstract class Status {
     var name : String = "Empty"
     var duration : Int = 0
     var durationLeft : Int = 0
+    def onAdd : Unit = {}
+    def onDelete : Unit = {}
+    def onEndTurn : Unit = {}
 }
 
 class NoStatus extends Status {
