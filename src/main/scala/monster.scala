cd6f044d9bc9138e64973d06db0c8b37e9a48b6c
zBlxst <tomloulou@yahoo.fr>
2022-01-29 14:20:41 +0100

    improved monster

--- src/main/scala/monster.scala
+++ src/main/scala/monster.scala
@@ -8,30 +8,87 @@ abstract class Monster {
     var defenseStat : Int = 100
     var speedStat : Int = 100
 
+    var attackBattle : Int = 100
+    var defenseBattle : Int = 100
+    var speedBattle : Int = 100
+
+    var accuracyBattle : Float = 1
+    var evasionBattle : Float = 1
+
     var hpMaxPerLevel : Int = 10
 
     var attackStage : Int = 0
     var defenseStage : Int = 0
     var speedStage : Int = 0
+    var accuracyStage : Int = 0
+    var evasionStage : Int = 0
 
     var statuts : Array[Status] = new Array[Status](10)
+    var attacks : Array[Attack] = new Array[Attack](4)
 
     val monsterType : Type
     var name : String = ""
 
     def originalName : String = {this.getClass.getSimpleName}
     def typeName : String = {monsterType.name}
+
     def castAttack (attack : Attack, other : Monster) : Boolean = {
-        if (scala.util.Random.nextFloat() <= attack.accuracy) {
+        var random = scala.util.Random.nextFloat()
+        var thisAccuracyEff = this.accuracyBattle * calcModifier(this, "accuracy")
+        var otherEvasionEff = other.evasionBattle * calcModifier(other, "evasion")
+        if (random <= attack.accuracy*thisAccuracyEff*otherEvasionEff) {
             other.receiveAttack(attack, this); true
-        } else {false}
+        } else {
+            if (random <= attack.accuracy) {
+                println("Attack missed")
+            } else if (random <= attack.accuracy*thisAccuracyEff) {
+                println("You missed your attack")
+            } else {
+                println("He dodged")
+            }
+            false
+        }
+    }
+
+    def calcModifier (monster : Monster, stat : String) : Int = {
+        var stage = {stat match {
+            case "attack" => monster.attackStage
+            case "defense" => monster.defenseStage
+            case "speed" => monster.speedStage
+            case "accuracy" => monster.accuracyStage
+            case "evasion" => monster.evasionStage
+        }}
+
+        {stage match {
+            case -6 => 25/100
+            case -5 => 28/100
+            case -4 => 33/100
+            case -3 => 40/100
+            case -2 => 50/100
+            case -1 => 66/100
+            case 0 => 100/100
+            case 1 => 150/100
+            case 2 => 200/100
+            case 3 => 250/100
+            case 4 => 300/100
+            case 5 => 350/100
+            case 6 => 400/100
+        }}
+
     }
 
     def receiveAttack (attack : Attack, other : Monster) : Boolean = {
-        var otherAttackEff = other.attackStat + 
-        damage = (((2/5*other.level+2)*attack.power*other.attack/defense)/50+2)
-        takeDamage()
+        
+        var otherAttackEff = other.attackBattle * calcModifier(other, "attack")
+        var thisDefenseEff = defenseBattle * calcModifier(this, "defense")
 
+        var random = scala.util.Random.nextFloat()*38/255 + 217/255
+
+        var damage = ((((2/5*other.level+2)*attack.power*otherAttackEff/thisDefenseEff)/50+2)*random).toInt
+
+        takeDamage(damage)
+        true
+        
     }
 
     def heal (amount : Int) : Unit = {
@@ -71,9 +128,6 @@ class Pikachu extends Monster {
     defenseStat = 40
     speedStat = 90
 
-    attackStatPerLevel = 10
-    defenseStatPerLevel = 10
-    hpMaxPerLevel = 10
     override val monsterType = Electric
     name = "Pikachuuuuu"
 }
@@ -85,9 +139,6 @@ class Carapuce extends Monster {
     defenseStat = 65
     speedStat = 50
 
-    attackStatPerLevel = 10
-    defenseStatPerLevel = 10
-    hpMaxPerLevel = 10
 
     override val monsterType = Water
     name = "Carapuuuuuce"
