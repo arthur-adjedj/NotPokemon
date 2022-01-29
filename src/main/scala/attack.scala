bad0f0598bbfee5c0bc977217ef7bff6e86b04a9
arthur-adjedj <arthur.adjedj@gmail.com>
2022-01-29 11:15:36 +0100

    Added attack class

---
+++ src/main/scala/attack.scala
@@ -0,0 +1,10 @@
+abstract class Attack {
+    var name : String = ""
+    var damage : Int = 0
+    var critChance : Float = 0
+    var speed : Int = 0
+    var accuracy : Float = 1
+    var attackType : Type     
+}
+
+
