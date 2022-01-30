e5b12de3988c5005baa10a702e213221dffa25cb
zBlxst <tomloulou@yahoo.fr>
2022-01-30 12:08:07 +0100

    swing works

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,12 @@
-object MyProgram {
-    def main(args: Array[String]): Unit = {
-        println( new Pikachu )
-        println(Agility)
-        println( new Carapuce )
+import swing._
+
+object MyApp extends SimpleSwingApplication {
+    def top = new MainFrame {
+        title = "My App"
+        contents = new BorderPanel {
+            add(new Label((new Pikachu).toString), BorderPanel.Position.North)
+            add(new Label(ShellSmash.toString), BorderPanel.Position.Center)
+            add(new Label((new Carapuce).toString), BorderPanel.Position.South)
+        }
     }
 }
