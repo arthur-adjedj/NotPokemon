de852f2d8f37c4ba6651524a23d22f1b7c773969
arthur-adjedj <arthur.adjedj@gmail.com>
2022-02-01 19:43:38 +0100

    added bars

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,13 +9,16 @@ class Battle extends Component {
   var battleBackgroundImg = javax.imageio.ImageIO.read(getClass.getResource("BattleBackground.png"))
   var pokemonFrontImg = javax.imageio.ImageIO.read(getClass.getResource("PikachuFront.png"))
   var pokemonBackImg = javax.imageio.ImageIO.read(getClass.getResource("SquirtleBack.png"))
+  var ennemyBarImg = javax.imageio.ImageIO.read(getClass.getResource("EnnemyBar.png"))
+  var yourBarImg = javax.imageio.ImageIO.read(getClass.getResource("YourBar.png"))
   override def paintComponent (g : Graphics2D) : Unit = {
     super.paintComponent(g)
     g.drawImage(battleBackgroundImg, 0, 0, null)
     g.drawImage(pokemonFrontImg,375,20,null)
     g.drawImage(pokemonBackImg,85,159,null)
+    g.drawImage(ennemyBarImg,10,20,null)
+    g.drawImage(yourBarImg,320,190,null)
   }
-
 }
 
 
@@ -24,10 +27,9 @@ object MyApp extends SimpleSwingApplication {
   
   var battle = new Battle
 
-
   def top = new MainFrame {
     title = "MyApplication"
     contents = battle
-    size = new Dimension(800, 800)
+    size = new Dimension(614, 574)
   }
 }
