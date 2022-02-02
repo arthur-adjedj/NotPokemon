891cc8422e7e73847c3c0d7f2dbdc4ead27bcade
zBlxst <tomloulou@yahoo.fr>
2022-02-02 18:22:19 +0100

    Add images refreshs

--- src/main/scala/battle.scala
+++ src/main/scala/battle.scala
@@ -8,13 +8,17 @@ class Battle (p1 : Player, p2 : Player) {
 
         p1.enterBattle
         p2.enterBattle
+
+        ui.updateImages
     }
 
     def start : Unit = {
         while (p1.playing && p2.playing) {
             p1.newTurn
+            ui.updateImages
             if (p2.playing) {
                 p2.newTurn
+                ui.updateImages
             }
         }
     }
