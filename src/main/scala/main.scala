a11a639e35199b09cdfe6d46daa730a88150cb1b
zBlxst <tomloulou@yahoo.fr>
2022-02-08 15:27:40 +0100

    added a message when starting

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit
 
 object MyApp {
   def main(argv : Array[String]) : Unit = {
+    println("En cas de problème, rappelons que 1/2 = 0")
     var b = new Battle(FirstPlayer, SecondPlayer)
     
     b.initialise
