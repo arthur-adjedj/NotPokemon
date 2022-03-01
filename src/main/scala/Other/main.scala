dcd56359309f55c504314be14fde0aeaa5b37294
zBlxst <tomloulou@yahoo.fr>
2022-03-01 18:27:21 +0100

    Chore : `Player` is faster on debug mode

--- src/main/scala/Other/main.scala
+++ src/main/scala/Other/main.scala
@@ -13,6 +13,7 @@ object Release {
 object Debug {
   def main(argv : Array[String]) : Unit = {
     Utils.debug = true
+    PlayerDisplayer.speed *= 3
     Utils.print("En cas de problème, rappelons que 1/2 = 0")
     var frame = new UI
     Utils.frame = frame
