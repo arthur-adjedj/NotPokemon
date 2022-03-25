6db1f7995cb7a1df165b1c0bca4983c90b36a5b5
arthur-adjedj <arthur.adjedj@gmail.com>
2022-03-25 19:24:37 +0100

    Chore : merged the two monster files

--- src/main/scala/Other/monsters.scala
+++ src/main/scala/Other/monsters.scala
@@ -820,3 +820,786 @@ class Gloom extends Monster {
 
 }
 
+class Vileplume extends Monster {
+    height = "1.2 m"
+    weight = "18.6 kg"
+
+    catchRate = 45
+
+    baseHpStat = 75
+    baseAttackStat = 80
+    baseDefenseStat = 85
+    baseSpeedStat = 50
+
+    xpGraph = "Medium Slow"
+    baseXp = 221
+
+    monsterType = Grass
+    name = "Vileplume"
+    originalName = "Vileplume"
+
+    attacks(0) = Growl
+    attacks(1) = Tackle
+    attacks(2) = VineWhip
+    attacks(3) = Growth
+    uiYShift = -4
+
+    description = "The larger its petals, the more toxic pollen it contains. Its big head is heavy and hard to hold up."
+}
+
+class Bellsprout extends Monster {
+    height = "0.7 m"
+    weight = "4.0 kg"
+    
+    catchRate = 255
+
+    baseHpStat = 50
+    baseAttackStat = 75
+    baseDefenseStat = 35
+    baseSpeedStat = 40
+
+    xpGraph = "Medium Slow"
+    baseXp = 60
+
+    monsterType = Grass
+    name = "Bellsprout"
+    originalName = "Bellsprout"
+
+    attacks(0) = Growl
+    attacks(1) = Tackle
+    attacks(2) = VineWhip
+    attacks(3) = Growth
+    uiYShift = 11
+
+    description = "A carnivorous POKEMON that traps and eats bugs. It uses its root feet to soak up needed moisture."
+}
+
+class Weepinbell extends Monster {
+    height = "1.0 m"
+    weight = "6.4 kg"
+    
+    catchRate = 120
+
+    baseHpStat = 65
+    baseAttackStat = 90
+    baseDefenseStat = 50
+    baseSpeedStat = 55
+
+    xpGraph = "Medium Slow"
+    baseXp = 137
+
+    monsterType = Grass
+    name = "Weepinbell"
+    originalName = "Weepinbell"
+
+    attacks(0) = Growl
+    attacks(1) = Tackle
+    attacks(2) = VineWhip
+    attacks(3) = Growth
+    uiYShift = 11
+
+    description = "It spits out POISONPOWDER to immobilize the enemy and then finishes it with a spray of ACID."
+}
+
+class Victreebel extends Monster {
+    height = "1.7 m"
+    weight = "15.5 kg"
+    
+    catchRate = 45
+
+    baseHpStat = 80
+    baseAttackStat = 105
+    baseDefenseStat = 65
+    baseSpeedStat = 70
+
+    xpGraph = "Medium Slow"
+    baseXp = 221
+
+    monsterType = Grass
+    name = "Victreebel"
+    originalName = "Victreebel"
+
+    attacks(0) = Growl
+    attacks(1) = Tackle
+    attacks(2) = VineWhip
+    attacks(3) = Growth
+    uiYShift = 2
+
+    description = "Said to live in huge colonies deep in jungles, although no one has ever returned from there."
+}
+
+class Exeggcute extends Monster {
+    height = "0.4 m"
+    weight = "2.5 kg"
+    
+    catchRate = 90
+
+    baseHpStat = 60
+    baseAttackStat = 40
+    baseDefenseStat = 80
+    baseSpeedStat = 40
+
+    xpGraph = "Slow"
+    baseXp = 65
+
+    monsterType = Grass
+    name = "Exeggcute"
+    originalName = "Exeggcute"
+
+    attacks(0) = Growl
+    attacks(1) = Tackle
+    attacks(2) = VineWhip
+    attacks(3) = Growth
+    uiYShift = 29
+
+    description = "Often mistaken for eggs. When disturbed, they quickly gather and attack in swarms."
+}
+
+class Exeggutor extends Monster {
+    height = "2.0 m"
+    weight = "120.0 kg"
+    
+    catchRate = 45
+
+    baseHpStat = 95
+    baseAttackStat = 95
+    baseDefenseStat = 85
+    baseSpeedStat = 55
+
+    xpGraph = "Slow"
+    baseXp = 186
+
+    monsterType = Grass
+    name = "Exeggutor"
+    originalName = "Exeggutor"
+
+    attacks(0) = Growl
+    attacks(1) = Tackle
+    attacks(2) = VineWhip
+    attacks(3) = Growth
+    uiYShift = 2
+
+    description = "Legend has it that on rare occasions, one of its heads will drop off and continue on as an EXEGGCUTE."
+}
+
+class Tangela extends Monster {
+    height = "1.0 m"
+    weight = "35.0 kg"
+    
+    catchRate = 45
+
+    baseHpStat = 65
+    baseAttackStat = 55
+    baseDefenseStat = 115
+    baseSpeedStat = 60
+
+    xpGraph = "Medium Fast"
+    baseXp = 87
+
+    monsterType = Grass
+    name = "Tangela"
+    originalName = "Tangela"
+
+    attacks(0) = Growl
+    attacks(1) = Tackle
+    attacks(2) = VineWhip
+    attacks(3) = Growth
+    uiYShift = 33
+
+    description = "The whole body is swathed with wide vines that are similar to seaweed. Its vines shake as it walks."
+}
+
+class Charmander extends Monster {
+    height = "0.6 m"
+    weight = "8.5 kg"
+
+    catchRate = 45
+
+    baseHpStat = 39
+    baseAttackStat = 52
+    baseDefenseStat = 43
+    baseSpeedStat = 65
+
+    xpGraph = "Medium Slow"
+    baseXp = 62
+
+    monsterType = Fire
+    name = "Salameche"
+    originalName = "Charmander"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = 5
+
+    description = "Obviously prefers hot places. When it rains, steam is said to spout from the tip of its tail."
+}
+
+class Charmeleon extends Monster {
+    height = "1.1 m"
+    weight = "19.0 kg"
+
+    catchRate = 45
+
+    baseHpStat = 58
+    baseAttackStat = 64
+    baseDefenseStat = 58
+    baseSpeedStat = 80
+
+    xpGraph = "Medium Slow"
+    baseXp = 142
+
+    monsterType = Fire
+    name = "Charmeleon"
+    originalName = "Charmeleon"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = -2
+
+    description = "When it swings its burning tail, it elevates the temperature to unbearably high levels."
+}
+
+class Charizard extends Monster {
+    height = "1.7 m"
+    weight = "90.5 kg"
+
+    catchRate = 45
+
+    baseHpStat = 78
+    baseAttackStat = 84
+    baseDefenseStat = 78
+    baseSpeedStat = 100
+
+    xpGraph = "Medium Slow"
+    baseXp = 240
+
+    monsterType = Fire
+    name = "Charizard"
+    originalName = "Charizard"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = -16
+
+    description = "Spits fire that is hot enough to melt boulders. Known to cause forest fires unintentionally."
+}
+
+class Vulpix extends Monster {
+    height = "0.6 m"
+    weight = "9.9 kg"
+
+    catchRate = 190
+
+    baseHpStat = 38
+    baseAttackStat = 41
+    baseDefenseStat = 40
+    baseSpeedStat = 65
+
+    xpGraph = "Medium Fast"
+    baseXp = 60
+
+    monsterType = Fire
+    name = "Vulpix"
+    originalName = "Vulpix"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = 5
+
+    description = "At the time of birth, it has just one tail. The tail splits from its tip as it grows older."
+}
+
+class Ninetales extends Monster {
+    height = "1.1 m"
+    weight = "19.9 kg"
+
+    catchRate = 75
+
+    baseHpStat = 73
+    baseAttackStat = 76
+    baseDefenseStat = 75
+    baseSpeedStat = 100
+
+    xpGraph = "Medium Fast"
+    baseXp = 177
+
+    monsterType = Fire
+    name = "Ninetales"
+    originalName = "Ninetales"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = -2
+
+    description = "Very smart and very vengeful. Grabbing one of its many tails could result in a 1000-year curse."
+}
+
+class Growlithe extends Monster {
+    height = "0.7 m"
+    weight = "19.0 kg"
+
+    catchRate = 190
+
+    baseHpStat = 55
+    baseAttackStat = 70
+    baseDefenseStat = 45
+    baseSpeedStat = 60
+
+    xpGraph = "Slow"
+    baseXp = 70
+
+    monsterType = Fire
+    name = "Growlithe"
+    originalName = "Growlithe"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = 2
+
+    description = "Very protective of its territory. It will bark and bite to repel intruders from its space."
+}
+
+class Arcanine extends Monster {
+    height = "1.9 m"
+    weight = "155.0 kg"
+
+    catchRate = 75
+
+    baseHpStat = 90
+    baseAttackStat = 110
+    baseDefenseStat = 80
+    baseSpeedStat = 95
+
+    xpGraph = "Slow"
+    baseXp = 194
+
+    monsterType = Fire
+    name = "Arcanine"
+    originalName = "Arcanine"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = -8
+
+    description = "A POKEMON that has been admired since the past for its beauty. It runs agilely as if on wings."
+}
+
+class Ponyta extends Monster {
+    height = "1.0 m"
+    weight = "30.0 kg"
+
+    catchRate = 190
+
+    baseHpStat = 50
+    baseAttackStat = 85
+    baseDefenseStat = 55
+    baseSpeedStat = 90
+
+    xpGraph = "Medium Fast"
+    baseXp = 82
+
+    monsterType = Fire
+    name = "Ponyta"
+    originalName = "Ponyta"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = -8
+
+    description = "Its hooves are 10 times harder than diamonds. It can trample anything completely flat in little time."
+}
+
+class Rapidash extends Monster {
+    height = "1.7 m"
+    weight = "95.0 kg"
+
+    catchRate = 60
+
+    baseHpStat = 65
+    baseAttackStat = 100
+    baseDefenseStat = 70
+    baseSpeedStat = 105
+
+    xpGraph = "Medium Fast"
+    baseXp = 175
+
+    monsterType = Fire
+    name = "Rapidash"
+    originalName = "Rapidash"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = -18
+
+    description = "Very competitive, this POKEMON will chase anything that moves fast in the hopes of racing it."
+}
+
+class Magmar extends Monster {
+    height = "1.3 m"
+    weight = "44.5 kg"
+
+    catchRate = 45
+
+    baseHpStat = 65
+    baseAttackStat = 95
+    baseDefenseStat = 57
+    baseSpeedStat = 93
+
+    xpGraph = "Medium Fast"
+    baseXp = 173
+
+    monsterType = Fire
+    name = "Magmar"
+    originalName = "Magmar"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = -12
+
+    description = "Its body always burns with an orange glow that enables it to hide perfectly among flames."
+}
+
+class Flareon extends Monster {
+    height = "0.9 m"
+    weight = "25.0 kg"
+
+    catchRate = 45
+
+    baseHpStat = 65
+    baseAttackStat = 130
+    baseDefenseStat = 60
+    baseSpeedStat = 65
+
+    xpGraph = "Medium Fast"
+    baseXp = 184
+
+    monsterType = Fire
+    name = "Flareon"
+    originalName = "Flareon"
+
+    attacks(0) = Growl
+    attacks(1) = Scratch
+    attacks(2) = Ember
+    attacks(3) = Flamethrower
+    uiYShift = 0
+
+    description = "When storing thermal energy in its body, its temperature could soar to over 1600 degrees."
+}
+
+
+class Rattata extends Monster {
+    height = "0.3 m"
+    weight = "3.5 kg"
+
+    catchRate = 255
+
+    baseHpStat = 30
+    baseAttackStat = 56
+    baseDefenseStat = 35
+    baseSpeedStat = 72
+
+    xpGraph = "Medium Fast"
+    baseXp = 51
+
+    monsterType = Normal
+    name = "Ratatata"
+    originalName = "Rattata"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 0
+
+    description = "Bites anything when it attacks. Small and very quick, it is a common sight in many places."
+}
+
+class Raticate extends Monster {
+    height = "0.7 m"
+    weight = "18.5 kg"
+
+    catchRate = 127
+
+    baseHpStat = 55
+    baseAttackStat = 81
+    baseDefenseStat = 60
+    baseSpeedStat = 97
+
+    xpGraph = "Medium Fast"
+    baseXp = 145
+
+    monsterType = Normal
+    name = "Raticate"
+    originalName = "Raticate"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 7
+
+    description = "It uses its whiskers to maintain its balance. It apparently slows down if they are cut off."
+}
+
+class Meowth extends Monster {
+    height = "0.4 m"
+    weight = "4.2 kg"
+
+    catchRate = 255
+
+    baseHpStat = 40
+    baseAttackStat = 45
+    baseDefenseStat = 30
+    baseSpeedStat = 90
+
+    xpGraph = "Medium Fast"
+    baseXp = 58
+
+    monsterType = Normal
+    name = "Meowth"
+    originalName = "Meowth"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = -4
+
+    description = "Adores circular objects. Wanders the streets on a nightly basis to look for dropped loose change."
+}
+
+class Persian extends Monster {
+    height = "1.0 m"
+    weight = "32.0 kg"
+
+    catchRate = 90
+
+    baseHpStat = 65
+    baseAttackStat = 70
+    baseDefenseStat = 60
+    baseSpeedStat = 115
+
+    xpGraph = "Medium Fast"
+    baseXp = 154
+
+    monsterType = Normal
+    name = "Persian"
+    originalName = "Persian"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 0
+
+    description = "Although its fur has many admirers, it is tough to raise as a pet because of its fickle meanness."
+}
+
+class Lickitung extends Monster {
+    height = "1.2 m"
+    weight = "65.5 kg"
+
+    catchRate = 45
+
+    baseHpStat = 90
+    baseAttackStat = 55
+    baseDefenseStat = 75
+    baseSpeedStat = 30
+
+    xpGraph = "Medium Fast"
+    baseXp = 77
+
+    monsterType = Normal
+    name = "Lickitung"
+    originalName = "Lickitung"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 15
+
+    description = "Its tongue can be extended like a chameleon’s. It leaves a tingling sensation when it licks enemies."
+}
+
+class Chansey extends Monster {
+    height = "1.1 m"
+    weight = "34.6 kg"
+
+    catchRate = 30
+
+    baseHpStat = 250
+    baseAttackStat = 5
+    baseDefenseStat = 5
+    baseSpeedStat = 50
+
+    xpGraph = "Fast"
+    baseXp = 395
+
+    monsterType = Normal
+    name = "Chansey"
+    originalName = "Chansey"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 11
+
+    description = "A rare and elusive POKEMON that is said to bring happiness to those who manage to get it."
+}
+
+class Kangaskhan extends Monster {
+    height = "2.2 m"
+    weight = "80.0 kg"
+
+    catchRate = 45
+
+    baseHpStat = 105
+    baseAttackStat = 95
+    baseDefenseStat = 80
+    baseSpeedStat = 90
+
+    xpGraph = "Medium Fast"
+    baseXp = 172
+
+    monsterType = Normal
+    name = "Kangaskhan"
+    originalName = "Kangaskhan"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = -4
+
+    description = "The infant rarely ventures out of its mother’s protective pouch until it is 3 years old."
+}
+
+class Tauros extends Monster {
+    height = "1.4 m"
+    weight = "88.4 kg"
+
+    catchRate = 45
+
+    baseHpStat = 75
+    baseAttackStat = 100
+    baseDefenseStat = 95
+    baseSpeedStat = 110
+
+    xpGraph = "Slow"
+    baseXp = 172
+
+    monsterType = Normal
+    name = "Tauros"
+    originalName = "Tauros"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 15
+
+    description = "When it targets an enemy, it charges furiously while whipping its body with its long tails."
+}
+
+class Eevee extends Monster {
+    height = "0.3 m"
+    weight = "6.5 kg"
+
+    catchRate = 45
+
+    baseHpStat = 55
+    baseAttackStat = 55
+    baseDefenseStat = 50
+    baseSpeedStat = 55
+
+    xpGraph = "Medium Fast"
+    baseXp = 65
+
+    monsterType = Normal
+    name = "Eevee"
+    originalName = "Eevee"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 5
+
+    description = "Its genetic code is irregular. It may mutate if it is exposed to radiation from element STONEs."
+}
+
+class Porygon extends Monster {
+    height = "0.8 m"
+    weight = "36.5 kg"
+
+    catchRate = 45
+
+    baseHpStat = 65
+    baseAttackStat = 60
+    baseDefenseStat = 70
+    baseSpeedStat = 40
+
+    xpGraph = "Medium Fast"
+    baseXp = 79
+
+    monsterType = Normal
+    name = "Porygon"
+    originalName = "Porygon"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 15
+
+    description = "A POKEMON that consists entirely of programming code. Capable of moving freely in cyberspace."
+}
+
+class Snorlax extends Monster {
+    height = "2.1 m"
+    weight = "460.0 kg"
+
+    catchRate = 25
+
+    baseHpStat = 160
+    baseAttackStat = 110
+    baseDefenseStat = 65
+    baseSpeedStat = 30
+
+    xpGraph = "Slow"
+    baseXp = 189
+
+    monsterType = Normal
+    name = "Snorlax"
+    originalName = "Snorlax"
+
+    attacks(0) = Tackle
+    attacks(1) = TailWhip
+    attacks(2) = QuickAttack
+    attacks(3) = Crunch
+    uiYShift = 15
+
+    description = "Very lazy. Just eats and sleeps. As its rotund bulk builds, it becomes steadily more slothful."
+}
