7d7e907988ba062d2b0d779e4f964953e1ea4b6b
zBlxst <tomloulou@yahoo.fr>
2022-03-25 19:59:01 +0100

    Fix : monsters are seen when entering the field

--- src/main/scala/Battle+Map/monster.scala
+++ src/main/scala/Battle+Map/monster.scala
@@ -88,7 +88,6 @@ abstract class Monster extends Object with ScoreForStrategy {
     def typeName : String = {monsterType.name}
 
     def enterBattle : Unit = {
-        Utils.frame.pokedexPane.pokemonArray.foreach(x => if (x.originalName == originalName) x.wasSeen = true)
 
         attackBattle = attackStat
         defenseBattle = defenseStat
@@ -109,6 +108,7 @@ abstract class Monster extends Object with ScoreForStrategy {
     }
 
     def enterField : Unit = {
+        Utils.frame.pokedexPane.pokemonArray.foreach(x => if (x.originalName == originalName) x.wasSeen = true)
         newMonsterSeen(owner.opponent.currentMonster)
         owner.opponent.currentMonster.newMonsterSeen(this)
         turnsOnField = 0
