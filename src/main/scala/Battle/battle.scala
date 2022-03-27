923af6bba479a4c551a2a34542bd7870a93bad3c
zBlxst <tomloulou@yahoo.fr>
2022-03-27 18:03:01 +0200

    Fix : a captured pokémon cannot attack

--- src/main/scala/Battle/battle.scala
+++ src/main/scala/Battle/battle.scala
@@ -34,18 +34,21 @@ class Battle (p1 : Character, p2 : Character) extends Thread {
             p2.newTurn
             var monsterP1 = p1.currentMonster
             var monsterP2 = p2.currentMonster
-            if (p1.currentAttack.priority > p2.currentAttack.priority || 
-                (p1.currentAttack.priority == p2.currentAttack.priority && p1.currentMonster.getSpeed >= p2.currentMonster.getSpeed)) {
-                p1.castAttack(p1.currentAttack)
-                TimeUnit.SECONDS.sleep(1)
-                if (monsterP2.alive && p2.playing) {
-                    p2.castAttack(p2.currentAttack)
-                }
-            } else {
-                p2.castAttack(p2.currentAttack)
-                TimeUnit.SECONDS.sleep(1)
-                if (monsterP1.alive && p1.playing) {
+            if (p1.playing && p2.playing) {
+                
+                if (p1.currentAttack.priority > p2.currentAttack.priority || 
+                    (p1.currentAttack.priority == p2.currentAttack.priority && p1.currentMonster.getSpeed >= p2.currentMonster.getSpeed)) {
                     p1.castAttack(p1.currentAttack)
+                    TimeUnit.SECONDS.sleep(1)
+                    if (monsterP2.alive && p2.playing) {
+                        p2.castAttack(p2.currentAttack)
+                    }
+                } else {
+                    p2.castAttack(p2.currentAttack)
+                    TimeUnit.SECONDS.sleep(1)
+                    if (monsterP1.alive && p1.playing) {
+                        p1.castAttack(p1.currentAttack)
+                    }
                 }
             }
             p1.currentAttack = EmptyAttack
